6fe209a505404defb9b598d8eb231165b3ac2e27
dionisio300 <dionisioassis80@gmail.com>
2026-05-20 18:49:01 -0300

    aula 30

---
+++ Aula30/aula30/src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='row justify-content-center'>
+            <div className='col-5'>
+                <div className='area-login'>
+                    <div className='icone-login'>
+                        <i className=''></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Login
