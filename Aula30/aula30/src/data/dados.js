6e39e2cedad55896ab4472e6a91c67468ccb1088
dionisio300 <dionisioassis80@gmail.com>
2026-05-20 19:06:24 -0300

    aula 30

---
+++ Aula30/aula30/src/data/dados.js
@@ -0,0 +1,14 @@
+let usuarios = [
+    {
+        login:'admin',
+        senha:'admin',
+        nome:'administrador'
+    },
+    {
+        login:'joao',
+        senha:'1234',
+        nome:'João'
+    }
+]
+
+export default usuarios
--- Aula30/aula30/src/pages/Login/Login.jsx
+++ Aula30/aula30/src/pages/Login/Login.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
+
   return (
     <>
     <div className='container'>
-        <div className='row justify-content-center'>
-            <div className='col-5'>
-                <div className='area-login'>
-                    <div className='icone-login'>
-                        <i className=''></i>
+        <div id='area-login' className='row justify-content-center align-items-center'>
+            <div  className='col-5'>
+                <div className='area-login border border-2 rounded p-3'>
+                    <div className='icone-login text-center'>
+                        <i style={{fontSize:'130px'}} class="bi bi-person-circle "></i>
+                    </div>
+                    <div className='form-login'>
+                        <div className='d-flex flex-column'>
+                            <label htmlFor="">Login: </label>
+                            <input type="text" />
+                        </div>
+                        <div className='d-flex flex-column'>
+                            <label htmlFor="">Senha: </label>
+                            <input type="password" />
+                        </div>
+                        <div className='mt-2'>
+                            <button className='btn btn-success'>Entrar</button>
+                        
+                        </div>
                     </div>
                 </div>
             </div>
