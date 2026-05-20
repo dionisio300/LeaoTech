01ce0d00f01bc34979906b9d140e54da88e340db
dionisio300 <dionisioassis80@gmail.com>
2026-05-20 19:21:16 -0300

    aula 30

--- Aula30/aula30/src/data/dados.js
+++ Aula30/aula30/src/data/dados.js
@@ -1,10 +1,12 @@
 let usuarios = [
     {
+        id:1,
         login:'admin',
         senha:'admin',
         nome:'administrador'
     },
     {
+        id:2,
         login:'joao',
         senha:'1234',
         nome:'João'
--- Aula30/aula30/src/pages/Login/Login.jsx
+++ Aula30/aula30/src/pages/Login/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import usuarios from '../../data/dados'
+
+
 const Login = () => {
+
+    let [login, setLogin] = useState('')
+    let [senha, setSenha] = useState('')
+    let id = ''
+
+    function verificarLogin(){
+
+        usuarios.forEach((usuario) => {
+            if (usuario.login == login && usuario.senha == senha){
+                id = usuario.id
+            }
+        })
+        if (id != ''){
+            console.log('Usuário encontrado!')
+        }else{
+            console.log('Usuário não encontrado')
+        }
+    }
 
   return (
     <>
@@ -9,20 +30,19 @@ const Login = () => {
             <div  className='col-5'>
                 <div className='area-login border border-2 rounded p-3'>
                     <div className='icone-login text-center'>
-                        <i style={{fontSize:'130px'}} class="bi bi-person-circle "></i>
+                        <i style={{fontSize:'130px'}} className="bi bi-person-circle "></i>
                     </div>
                     <div className='form-login'>
                         <div className='d-flex flex-column'>
                             <label htmlFor="">Login: </label>
-                            <input type="text" />
+                            <input type="text" onChange={(e) => setLogin(e.target.value)} />
                         </div>
                         <div className='d-flex flex-column'>
                             <label htmlFor="">Senha: </label>
-                            <input type="password" />
+                            <input onChange={(e) => setSenha(e.target.value)} type="password" />
                         </div>
                         <div className='mt-2'>
-                            <button className='btn btn-success'>Entrar</button>
-                        
+                            <button onClick={verificarLogin} className='btn btn-success'>Entrar</button>
                         </div>
                     </div>
                 </div>
